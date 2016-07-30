d88f967b1d76323188a67a062c61b0c0e9495d84
David Moran <david@dmoran.es>
2016-07-30 10:14:26 +0200

    restructured cmake files

--- src/freebsd-free.cmake
+++ src/freebsd-free.cmake
@@ -1,5 +1,4 @@
-cmake_minimum_required(VERSION 3.5)
 project(freebsd-free)
 add_compile_options(-std=c++1y)
-add_executable(freebsd-free main.cc memory.cc sysctl.cc)
+add_executable(freebsd-free src/main.cc src/memory.cc src/sysctl.cc)
 target_link_libraries(freebsd-free kvm)
